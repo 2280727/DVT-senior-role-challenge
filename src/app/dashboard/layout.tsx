f9b0ddb4ec5de384477cc854cd86f3d43cd390d0
2280727 <p.creator44@gmail.com>
2024-03-27 12:21:13 +0200

    Update website layout: Adjust top navigation and overall layout

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,11 @@
+import TopNav from "../ui/dashboard/topnav";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+    return(
+        <div className="flex h-screen flex-col ">
+            <div className="flex-none ">
+                <TopNav/>
+            </div>
+        </div>
+    );
+} 
