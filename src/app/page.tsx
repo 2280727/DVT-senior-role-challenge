fe80dd2cd15baffc814967c1e5e37adac71223f3
2280727 <p.creator44@gmail.com>
2024-04-02 16:00:31 +0200

    chore: the int route should be the dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
+"use client"
 import Link from 'next/link'
+import { redirect } from 'next/navigation'
+import React from 'react'
  
 export default function Page() {
+
+  React.useEffect(()=> {
+    redirect('/dashboard')
+  },[])
   return (
     <div>
       <h1>Home</h1>
